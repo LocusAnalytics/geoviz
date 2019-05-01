5c9d59e2625734c5a101fef24a1b7aaf0e2d8964
aaronwflee <alee@locus.co>
2019-05-01 10:50:30 -0400

    update params -- allow text style and color customization, change palette2 to use Locus brand color

--- geoviz/src/params.py
+++ geoviz/src/params.py
@@ -9,11 +9,16 @@ def get_palette_colors(palette_label, ncolors):
     return palette
 
 DEFAULTFORMAT = {## plot properties
-          'title':'', 'ht':800, 'wt':1000, 'background_color':None,
+          'ht':600, 'wt':1000, 'background_color':None,
+          'title':'', 'font':'futura', 'title_fontsize':None,
+          'tools':'zoom_in,zoom_out,pan,reset,save',
           ## main map properties
-          'fill_alpha':0.8, 'line_color':'#d3d3d3', 'line_width':0.5,
+          'fill_alpha':0.8, 'line_color':'#d3d3d3', 'line_width':0.5, 'simplify':0,
           ## color bar properties
-          'lin_or_log':'lin', 'ncolors':7, 'palette':1, 'cbar_text':'0,000',
+          'lin_or_log':'lin', 'ncolors':7, 'palette':1,
+          'cbar_fontsize':None, 'cbar_textfmt':'0,000',
+          'cbar_title':'', 'cbar_title_align':'center','cbar_style':None,
+          'cbar_tick_color':'black', 'cbar_tick_alpha':1,
           ## state map properties
           'st_alpha':1, 'st_fill':None, 'st_line_color':'black', 'st_line_width':1}
 
@@ -34,3 +39,6 @@ for ptype in palette_dict:
             palette = get_palette_colors(label, n)
             COLORS[ptype][label][n] = palette
             COLORS[ptype][rank+1][n] = palette
+
+for i in COLORS['sequential'][2]:
+    COLORS['sequential'][2][i][-1] = '#0a3959'
