1b115d5feff700b8d354b9be9c04d18b28cc3aa3
aaronwflee <alee@locus.co>
2019-05-22 17:01:36 -0400

    clean up code
    - rename main function to be plot() instead of choropleth_county()
    - corresponding renaming of bokeh figure to be bkplot instead of plot
    - commit leftover changes relevant to splitting script into separate script

--- geoviz/src/choropleth.py
+++ geoviz/src/choropleth.py
@@ -5,63 +5,51 @@ from bokeh import plotting, models, io
 
 
 def initialize_plot(formatting):
-    """ Create Bokeh figure
+    """ Create Bokeh figure to which glyphs/elements can be added.
 
     :param (dict) formatting: see DEFAULTFORMAT from params.py
     :return: Bokeh figure object """
 
-    plot = plotting.figure(title=formatting['title'],
-                           background_fill_color=formatting['background_color'],
-                           plot_width=formatting['width'],
-                           plot_height=formatting['width']*HEIGHT_RATIO,
-                           tools=formatting['tools'])
-    plot.title.text_font = formatting['font']
-    plot.title.text_font_size = formatting['title_fontsize']
-    plot.grid.grid_line_color = None
-    plot.axis.visible = False
-    plot.border_fill_color = None
-    plot.outline_line_color = None
+    bkplot = plotting.figure(title=formatting['title'],
+                             background_fill_color=formatting['background_color'],
+                             plot_width=formatting['width'],
+                             plot_height=int(formatting['width']*HEIGHT_RATIO),
+                             tools=formatting['tools'])
+    bkplot.title.text_font = formatting['font']
+    bkplot.title.text_font_size = formatting['title_fontsize']
+    bkplot.grid.grid_line_color = None
+    bkplot.axis.visible = False
+    bkplot.border_fill_color = None
+    bkplot.outline_line_color = None
     return plot
 
 
-def draw_state(plot, formatting):
-    """ Adds a state choropleth (default is transparent fill) to an existing Bokeh plot.
-
-    :param (Bokeh object) plot: pre-defined Bokeh figure
-    :param (dict) formatting: see DEFAULTFORMAT from params.py
-    :return: None (adds to Bokeh object) """
-
-    state_geojson = shape_geojson('state', formatting['simplify']).to_json()
-    state_source = models.GeoJSONDataSource(geojson=state_geojson)
-    plot.patches('xs', 'ys', source=state_source,
-                 fill_color=formatting['st_fill'], fill_alpha=formatting['st_alpha'],
-                 line_color=formatting['st_line_color'], line_width=formatting['st_line_width'])
-
-
-def draw_main(plot, geo_df, y_var, y_type, formatting):
+def draw_main(bkplot, geo_df, y_var, y_type, geolabel, formatting):
     """ Adds choropleth based on specified y_var to an existing Bokeh plot.
 
     :param (Bokeh object) plot: pre-defined Bokeh figure
     :param (gpd.DataFrame) geo_df: merged geopandas DataFrame from merge_to_geodf()
     :param (str) y_var: column name of variable to plot
     :param (str) y_type: 'sequential', 'divergent', or 'categorical' -- for palette
+    :param (str) geolabel: column name to use. default is county/state name from shapefile
     :param (dict) formatting: see DEFAULTFORMAT from params.py
     :return: None (adds to Bokeh object) """
 
     geo_src = models.GeoJSONDataSource(geojson=geo_df.to_json())
     cmap = make_color_mapper(geo_df[y_var], y_type, formatting)
 
-    shapes = plot.patches('xs', 'ys', fill_color={'field':y_var, 'transform': cmap},
-                          fill_alpha=formatting['fill_alpha'], line_color=formatting['line_color'],
-                          line_width=formatting['line_width'], source=geo_src)
+    shapes = bkplot.patches('xs', 'ys', fill_color={'field':y_var, 'transform': cmap},
+                            fill_alpha=formatting['fill_alpha'],
+                            line_color=formatting['line_color'],
+                            line_width=formatting['line_width'], source=geo_src)
 
     hover = models.HoverTool(renderers=[shapes])
-    hover.tooltips = [('name', '@name'),
+    hover.tooltips = [('name', f'@{geolabel}'),
                       (y_var, f'@{y_var}')]
-    plot.add_tools(hover)
 
     cbar = make_color_bar(cmap, formatting)
     plot.add_layout(cbar)
+    bkplot.add_tools(hover)
 
 
 def make_color_mapper(y_values, y_type, formatting):
@@ -72,11 +60,8 @@ def make_color_mapper(y_values, y_type, formatting):
     :param (dict) formatting: see DEFAULTFORMAT from params.py
     :return: Bokeh colormapper object """
 
-    c_min = formatting.get('cbar_min', min(y_values))
-    c_max = formatting.get('cbar_max', max(y_values))
-
     try:
-        palette = COLORS[y_type][formatting['palette']][formatting['ncolors']]
+        palette = PALETTES[y_type][formatting['palette']][formatting['ncolors']]
     except KeyError:
         palette = get_palette_colors(formatting['palette'], formatting['ncolors'])
 
@@ -94,8 +79,7 @@ def make_color_bar(cmap, formatting):
 
     color_bar = models.ColorBar(color_mapper=cmap, label_standoff=10, location='bottom_right',
                                 background_fill_color=None,
-                                formatter=models.NumeralTickFormatter(
-                                format=formatting['cbar_textfmt']),
+                                formatter=models.NumeralTickFormatter(format=formatting['cbar_textfmt']),
                                 major_label_text_font_size=formatting['cbar_fontsize'],
                                 major_label_text_font=formatting['font'],
                                 major_tick_line_color=formatting['cbar_tick_color'],
@@ -105,12 +89,26 @@ def make_color_bar(cmap, formatting):
                                 title_text_font=formatting['font'],
                                 title_text_align=formatting['cbar_title_align'],
                                 title_text_font_style=formatting['cbar_style'],
-                                title_standoff=int(formatting['width']*formatting['title_sf_ratio']))
+                                title_standoff=int(formatting['width']*formatting['title_sf']))
 
     return color_bar
 
 
-def draw_choropleth_layers(order, plot, geo_df, y_var, y_type, formatting):
+def draw_state(bkplot, formatting):
+    """ Adds a state choropleth (default is transparent fill) to an existing Bokeh plot.
+
+    :param (Bokeh object) plot: pre-defined Bokeh figure
+    :param (dict) formatting: see DEFAULTFORMAT from params.py
+    :return: None (adds to Bokeh object) """
+
+    state_geojson = prc.shape_geojson('state', formatting['simplify']).to_json()
+    state_source = models.GeoJSONDataSource(geojson=state_geojson)
+    bkplot.patches('xs', 'ys', source=state_source,
+                   fill_color=formatting['st_fill'], fill_alpha=formatting['st_alpha'],
+                   line_color=formatting['st_line_color'], line_width=formatting['st_line_width'])
+
+
+def draw_choropleth_layers(order, bkplot, geo_df, y_var, y_type, geolabel, formatting):
     """ Draws multi-layer choropleths (main + state outlines)
 
     :param (str) order: ['before', 'after', 'both']; order of drawing state outline
@@ -118,21 +116,22 @@ def draw_choropleth_layers(order, plot, geo_df, y_var, y_type, formatting):
     :param (gpd.DataFrame) geo_df: merged geopandas DataFrame from merge_to_geodf()
     :param (str) y_var: column name of variable to plot
     :param (str) y_type: 'sequential', 'divergent', or 'categorical' -- for palette
+    :param (str) geolabel: column name to use. default is county/state name from shapefile
     :param (dict) formatting: see DEFAULTFORMAT from params.py
     :return: None (adds to Bokeh object) """
 
     if order in ['before', 'both']:
-        draw_state(plot, formatting)
-    draw_main(plot, geo_df, y_var, y_type, formatting)
+        draw_state(bkplot, formatting)
+    draw_main(bkplot, geo_df, y_var, y_type, geolabel, formatting)
     if order == 'after':
-        draw_state(plot, formatting)
+        draw_state(bkplot, formatting)
     elif order == 'both':
         temp_formatting = formatting.copy()
         temp_formatting['st_fill'] = None
-        draw_state(plot, temp_formatting)
+        draw_state(bkplot, temp_formatting)
 
 
-def save_plot(plot, output):
+def save_plot(bkplot, output):
     """ Determines how choropleth plot is saved.
 
     :param (Bokeh object) plot: pre-defined Bokeh figure
@@ -144,54 +143,57 @@ def save_plot(plot, output):
 
     if output == 'html':
         io.output_file('bokeh_plot.html')
-        io.save(plot, 'bokeh_plot.html')
+        io.save(bkplot, 'bokeh_plot.html')
     else:
         io.output_notebook()
         if output == 'svg':
-            plot.output_backend = 'svg'
+            bkplot.output_backend = 'svg'
         plotting.show(plot)
 
 
-def choropleth_county(file_or_df, geoid_var, geoid_type, y_var, y_type, state_outline=None,
-                      formatting=None, output=False, dropna=True):
+def plot(file_or_df, geoid_var, geoid_type, y_var, y_type, state_outline=None,
+         geolvl='county', geolabel='name', formatting=None, output=False, dropna=True):
     """Short summary.
 
     :param (str/pd.DataFrame) file_or_df: csv filepath or pandas/geopandas DataFrame with geoid_var
     :param (str) geoid_var: name of column containing the geo ID to match on
-    :param (str) geoid_type: 'fips' (recommended), 'name', or 'abbrev'
+    :param (str) geoid_type: 'fips' (recommended), 'name', or 'abbrev' (state only)
     :param (str) y_var: column name of variable to plot
     :param (str) y_type: 'sequential', 'divergent', or 'categorical' -- for palette
     :param (str) state_outline: ['before','after','both','None']; when to plot state outline map
+    :param (str) geolvl: 'county' or 'state'
+    :param (str) geolabel: column name to use. default is county/state name from shapefile
     :param (dict) formatting: if custom dict is passed, update DEFAULTFORMAT with those key-values
     :param (str) output: see save_plot()
     :param (bool) dropna: default True, if false, keeps rows where y_var is nan.
     :return: if output is 'bokeh', returns Bokeh object; else None """
 
     ## get default plot formatting and update if necessary
-    FORMAT = DEFAULTFORMAT.copy()
+    temp_format = DEFAULTFORMAT.copy()
     if formatting:
-        FORMAT.update(formatting)
+        temp_format.update(formatting)
 
     ## process data
-    shape_df = shape_geojson('county', FORMAT['simplify'])
-    geo_df = merge_to_geodf(shape_df, file_or_df, geoid_var, geoid_type,
-                            geolvl='county', how_merge='inner')
+    shape_df = prc.shape_geojson(geolvl, temp_format['simplify'])
+    geo_df = prc.merge_to_geodf(shape_df, file_or_df, geoid_var, geoid_type,
+                                geolvl=geolvl, how_merge='inner')
+
     if dropna:
         geo_df = geo_df[geo_df[y_var].notnull()]
 
     ## plot and save choropleth
-    plot = initialize_plot(FORMAT)
-    draw_choropleth_layers(state_outline, plot, geo_df, y_var, y_type, FORMAT)
+    bkplot = initialize_plot(temp_format)
+    draw_choropleth_layers(state_outline, bkplot, geo_df, y_var, y_type, geolabel, temp_format)
 
     if output != 'bokeh':
-        save_plot(plot, output)
+        save_plot(bkplot, output)
         ## return to original state
         io.reset_output()
-        FORMAT = DEFAULTFORMAT.copy()
+        temp_format = DEFAULTFORMAT.copy()
     return plot
 
 
-def empty_map(geo='state', formatting=None, output='svg'):
+def plot_empty(geo='state', formatting=None, output='svg'):
     """Generate map outline (no fill).
 
     :param (str) geo: 'state' or 'county'
@@ -200,22 +202,22 @@ def empty_map(geo='state', formatting=None, output='svg'):
     :return: if output is 'bokeh', returns Bokeh object; else None """
 
     ## get default plot formatting and update if necessary
-    FORMAT = DEFAULTFORMAT.copy()
+    temp_format = DEFAULTFORMAT.copy()
     if formatting:
-        FORMAT.update(formatting)
+        temp_format.update(formatting)
 
     ## process data
-    shape_df = shape_geojson(geo, FORMAT['simplify'])
+    shape_df = prc.shape_geojson(geo, temp_format['simplify'])
     geo_src = models.GeoJSONDataSource(geojson=shape_df.to_json())
 
     ## plot and save choropleth
-    plot = initialize_plot(formatting)
-    plot.patches('xs', 'ys', fill_color=None, line_color=FORMAT['line_color'],
-                 line_width=FORMAT['line_width'], source=geo_src)
+    bkplot = initialize_plot(formatting)
+    bkplot.patches('xs', 'ys', fill_color=None, line_color=temp_format['line_color'],
+                   line_width=temp_format['line_width'], source=geo_src)
 
     if output != 'bokeh':
-        save_plot(plot, output)
+        save_plot(bkplot, output)
         ## return to original state
         io.reset_output()
-        FORMAT = DEFAULTFORMAT.copy()
-    return plot
+        temp_format = DEFAULTFORMAT.copy()
+    return bkplot
